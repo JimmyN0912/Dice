2b9990e9a8514dddab263b4b8629051ae0f064f4
JimmyN0912 <74365273+JimmyN0912@users.noreply.github.com>
2023-06-25 20:41:36 +0800

    Update 23-3.cpp

--- 23-3.cpp
+++ 23-3.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main(){
     int b[4][5],a,total,i,j; //x[銷售員數量][產品數量]
-    for(i=0;i<4;i++){   //第幾名銷售員
-        for(j=0;j<5;j++){  //第幾項商品
+    for(i=1;i<=4;i++){   //第幾名銷售員
+        for(j=1;j<=5;j++){  //第幾項商品
            std::cin >> a;
            b[i][j] = a;
            total +=a;
         }
     }
-    for(i=0;i<4;i++){  //第幾名銷售員
-        for(j=0;j<5;j++){  //第幾項商品
+    for(i=1;i<=4;i++){  //第幾名銷售員
+        for(j=1;j<=5;j++){  //第幾項商品
             std::cout << i+1 <<"號銷售員" << j +1<<"號產品" << b[i][j] << "元";
         }
     }
