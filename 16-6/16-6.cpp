fdd8e94b26910ebeac023234ca8174f5da3b237d
JimmyN0912 <74365273+JimmyN0912@users.noreply.github.com>
2023-06-30 22:57:05 +0800

    Create 16-6.cpp

---
+++ 16-6/16-6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <Windows.h>  //寫到Dice的時候這行不用
+using namespace std;
+int main(){
+    SetConsoleOutputCP(CP_UTF8); //寫到Dice的時候這行不用
+    int num;
+    for(int i=1;i<=5;i++){//重複5次
+        cin >> num;//輸入要判斷的數字
+        for(int j=1;j<=num;j++){//從1開始遞增判斷是不是因數，直到等於輸入值
+            if(num%j==0){//是因數就輸出
+                cout << j << endl;
+            }
+        }
+    }
+    return 0;
+}
